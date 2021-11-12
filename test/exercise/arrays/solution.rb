92490c210a9f3d2dbbbfd0d28ea4f3a9d5a157d4
alexey <alexey-melekhov1989@yandex.ru>
2021-11-12 22:37:49 +0500

    refactoring replace and search

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,44 +2,47 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        result = []
-        array.each do |el_source|
-          array.each do |el|
-            if el_source < el \
-              && el_source.positive?
-              el_source = el
-            end
+        positive_numbers = array.select { |num| num.positive? }
+        max_number = array.first
+        positive_numbers.each do |num|
+          if max_number < num
+            max_number = num
           end
-          result << el_source
         end
-        result
+        array.map do |num|
+          if num.positive?
+            num = max_number
+          else
+            num
+          end
+        end
       end
 
-      def search(_array, _query)
-        if has_element(_array, _query)
-          _array.find_index(_query)
+      def search(array, query)
+        if has_element(array, query)
+          array.find_index(query)
         else
           -1
         end
       end
       
-      def has_element(_array, _query)
-        if _array.empty?
+      def has_element(array, query)
+        if array.empty?
           return false
         end
         low = 0
-        high = _array.length
+        high = array.length
         mid = low + ((high - low) / 2)
-        quess = _array[mid]
-        if quess == _query
+        quess = array[mid]
+        if quess == query
           return true
         end
-        if quess > _query
+        if quess > query
           high = mid
         else
           low = mid + 1
         end
-        has_element(_array[low, high], _query)
+        has_element(array[low, high], query)
       end
       
     end
