1bd51645069d99edba10c3224a09bd090b4fcb1f
alexey <alexey-melekhov1989@yandex.ru>
2021-11-18 16:26:41 +0500

    add completed task on fp2

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,16 +5,43 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each; end
+      def my_each
+        for el in self
+          yield el
+        end
+      end
 
       # Написать свою функцию my_map
-      def my_map; end
+      def my_map
+        result = self.my_reduce([]) do |acc, el|
+          acc << yield(el)
+        end
+        self.class.new(result)
+      end
 
       # Написать свою функцию my_compact
-      def my_compact; end
+      def my_compact
+        result = self.my_reduce([]) do |acc, el|
+          if !el.nil?
+            acc << el
+          end
+          acc
+        end
+        self.class.new(result)
+      end
 
       # Написать свою функцию my_reduce
-      def my_reduce; end
+      def my_reduce(acc = nil, &block)
+        self.my_each do |el|
+          if acc.nil?
+            acc = el
+          else
+            acc = yield acc, el
+          end
+        end
+        acc
+      end
+
     end
   end
 end
